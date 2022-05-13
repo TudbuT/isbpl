3bbdf874f87049c8ab25bfa5a274b0713732151e
TudbuT <tudbut@tudbut.de>
2022-05-13 19:18:49 +0200

    move ISBPL.java to root

--- setup.sh
+++ setup.sh
@@ -2,11 +2,14 @@
 if [ "$ISBPL_PATH" = "" ] ; then
     echo 'ISBPL_PATH=/usr/lib/isbpl' >> /etc/environment
 fi
-mkdir /usr/lib/isbpl
-cp ./*.isbpl /usr/lib/isbpl
+mkdir /usr/lib/isbpl >& /dev/null
+cp ./*.isbpl /usr/lib/isbpl >& /dev/null
 cd bootstrap
+cp ../ISBPL.java .
+echo ">>> There will be warnings about the Unsafe. Sadly, the code producing them is required for JIO. <<<"
 javac ISBPL.java
-zip -r ISBPL.jar *.class META-INF 
+rm ISBPL.java ISBPL.jar
+zip -r ISBPL.jar * META-INF 
 rm *.class
 cd ..
 echo "#!/usr/bin/java -jar" > ISBPL.jar
