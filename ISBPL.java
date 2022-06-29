89191975fe73838847c5e20a8557bb4ad5ca0ac4
TudbuT <tudbut@tudbut.de>
2022-06-29 23:26:21 +0200

    add more separator chars

--- ISBPL.java
+++ ISBPL.java
@@ -1719,7 +1719,7 @@ public class ISBPL {
                 word.append('"');
                 isInString = true;
             }
-            else if(c == ' ' || c == '°' || c == 'ß' || c == '§') {
+            else if(c == ' ' || c == '\'' || c == ':' || c == '°' || c == 'ß' || c == '§') {
                 String w = word.toString();
                 while(w.startsWith("(") && w.length() > 1)
                     w = w.substring(1);
